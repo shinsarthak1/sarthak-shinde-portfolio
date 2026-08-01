1ea941503ba063b5df052cbc33fa6a5e6339c410
shinsarthak1 <shin.sarthak@gmail.com>
2026-08-01 17:29:31 +0530

    Testing completed

---
+++ script.js
@@ -0,0 +1,13 @@
+const typed = new Typed(".typing", {
+    strings: [
+        "Software Engineer!",
+        "Front-End Developer!",
+        "Problem Solver!"
+    ],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    showCursor: true,
+    cursorChar: "|"
+});
